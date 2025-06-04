3f2e557c4235fd3d645da967020c25bae3001d00
Kinshuk-Goel <kg467@outlook.com>
2025-06-03 22:22:27 -0400

    website logo update

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ const EnvironmentalCareersApp = () => {
     <>
       <Head>
         <title>Environmental Careers Guide</title>
-        <link rel="icon" type="image/png" href="../public/pine.png" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 16 16%22><text y=%2214%22 font-size=%2216%22>🌍</text></svg>" />
       </Head>
       <style jsx global>{`
         html {
